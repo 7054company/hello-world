5a411da7a45e8d5f42180bc419a4d8396020e199
ibrahimyks99 <54736860+ibrahimyks99@users.noreply.github.com>
2022-06-27 20:43:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.post('/', function(req, res) {
   res.send({ status: 'Success', message: 'Hello World' });
 });
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000!');
+app.listen(80, function () {
+  console.log('Listening on port 80!');
 });
